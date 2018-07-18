f00be77b449d5d238bffb47b89c2caa176fac9a0
Ann <735626035@qq.com>
2018-07-18 21:58:26 +0800

    初级算法-字符串

--- src/com/coder/初级算法/字符串/字符串中的第一个唯一字符.java
+++ src/com/coder/初级算法/字符串/字符串中的第一个唯一字符.java
@@ -1,5 +1,7 @@
 package com.coder.初级算法.字符串;
 
+import java.util.HashSet;
+
 public class 字符串中的第一个唯一字符 {
 
     /*
@@ -15,4 +17,22 @@ public class 字符串中的第一个唯一字符 {
     * 注意事项：您可以假定该字符串只包含小写字母。
     *
     * */
+
+    public static void main(String[] args) {
+        String s = "loveleetcode";
+        System.out.println(firstUniqChar(s));
+
+    }
+
+    public static int firstUniqChar(String s) {
+        int index = -1;
+        for(char c = 'a'; c <= 'z'; c++){
+            if(s.indexOf(c) == -1) continue;
+            if(s.indexOf(c) == s.lastIndexOf(c)){
+                index = (index == -1) ? s.indexOf(c) : Math.min(s.indexOf(c),index);
+            }
+        }
+        return index;
+    }
+
 }
