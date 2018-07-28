95d07f4ea2ba1298e0bfd635ec49200e8a43db90
吴俊龙 <735626035@qq.com>
2018-07-28 11:12:53 +0800

    初级算法-字符串

--- src/com/coder/初级算法/字符串/有效的字母异位词.java
+++ src/com/coder/初级算法/字符串/有效的字母异位词.java
@@ -8,7 +8,13 @@ public class 有效的字母异位词 {
 
     public static boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        // TODO
-        return  false;
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+        for(char c = 'a'; c<='z'; c++){
+            s = s.replaceAll(c + "","");
+            t = t.replaceAll(c + "","");
+            if(s.length() != t.length()) return false;
+        }
+        return true;
     }
 }
